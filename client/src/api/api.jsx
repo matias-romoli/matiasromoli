cb4d1c4892b6eff72fe1f944440ba77070f74051
matiasromoli <matiasromoli22@gmail.com>
2025-02-11 14:17:25 -0300

    changes

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export async function Posts() {
   try {
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}/posts`);
+    const response = await axios.get(
+      "https://matias-romoli.onrender.com/api/posts"
+    );
     return await response.data;
   } catch (error) {
     console.error("Error al obtener las publicaciones:", error);
